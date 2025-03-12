4e1cac1b201c6270cb04f647815867fddb7b0358
Nelly Delgado <Nelly Delgado@Nelly_Delgado.lan>
2025-03-12 09:13:36 -0500

    adelanto

---
+++ Lab2AppsRed/src/ejercicio8parte3/EchoTCPClient.java
@@ -0,0 +1,22 @@
+package ejercicio8parte3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class EchoTCPClient {
+	
+	public static void main(String[] args) throws IOException { 
+		Socket clientSideSocket = new Socket("localhost", 3400); 
+		PrintWriter toNetwork = new PrintWriter(clientSideSocket.getOutputStream(), true); 
+		BufferedReader fromNetwork = new BufferedReader(new 
+		InputStreamReader(clientSideSocket.getInputStream())); 
+		toNetwork.println("Hello World"); 
+		String fromServer = fromNetwork.readLine(); 
+		System.out.println("[Client] From server: " + fromServer); 
+		clientSideSocket.close(); 
+		} 
+
+}
---
+++ Lab2AppsRed/src/ejercicio8parte3/EchoTCPServer.java
@@ -0,0 +1,36 @@
+package ejercicio8parte3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoTCPServer {
+	
+	 public static void main(String[] args) throws IOException { 
+		    ServerSocket listener = new ServerSocket(3400); 
+		    System.out.println("The Echo TCP server is running on port 3400 ..."); 
+		 
+		    System.out.println("The server is waiting for a client."); 
+		    Socket serverSideSocket = listener.accept(); 
+		    System.out.println("A client has connected."); 
+		 
+		    BufferedReader fromNetwork = new BufferedReader(new  
+		      InputStreamReader(serverSideSocket.getInputStream())); 
+		 
+		    PrintWriter toNetwork = new PrintWriter(serverSideSocket.getOutputStream(), true); 
+		 
+		    String message = fromNetwork.readLine(); 
+		    System.out.println("[Server] From client: " + message); 
+		 
+		    String answer = message; 
+		 
+		    toNetwork.println(answer); 
+		    serverSideSocket.close(); 
+		 
+		    listener.close(); 
+		  } 
+
+}
